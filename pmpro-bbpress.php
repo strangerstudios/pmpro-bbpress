f4718f6097a97b4a0836a7ae1890bb90cf9abe60
Jason Coleman <jason@strangerstudios.com>
2014-01-10 11:58:06 -0500

    v1.0 updates from Jessica Oros. Showing message on levels page when redirecting away from a forum.

--- pmpro-bbpress.php
+++ pmpro-bbpress.php
@@ -54,14 +54,14 @@ function pmprobbp_tgmpa_register() {
 			'menu_title'                       			=> __( 'Install Plugins', $theme_text_domain ),
 			'installing'                       			=> __( 'Installing Plugin: %s', $theme_text_domain ), // %1$s = plugin name
 			'oops'                             			=> __( 'Something went wrong with the plugin API.', $theme_text_domain ),
-			'notice_can_install_required'     			=> _n_noop( 'PMPro bbPress requires the following plugin: %1$s.', 'This theme requires the following plugins: %1$s.' ), // %1$s = plugin name(s)
-			'notice_can_install_recommended'			=> _n_noop( 'PMPro bbPress recommends the following plugin: %1$s.', 'This theme recommends the following plugins: %1$s.' ), // %1$s = plugin name(s)
-			'notice_cannot_install'  					=> _n_noop( 'PMPro bbPress: Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.' ), // %1$s = plugin name(s)
-			'notice_can_activate_required'    			=> _n_noop( 'PMPro bbPress: The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.' ), // %1$s = plugin name(s)
-			'notice_can_activate_recommended'			=> _n_noop( 'PMPro bbPress: The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.' ), // %1$s = plugin name(s)
-			'notice_cannot_activate' 					=> _n_noop( 'PMPro bbPress: Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.' ), // %1$s = plugin name(s)
-			'notice_ask_to_update' 						=> _n_noop( 'PMPro bbPress: The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.' ), // %1$s = plugin name(s)
-			'notice_cannot_update' 						=> _n_noop( 'PMPro bbPress: Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.' ), // %1$s = plugin name(s)
+			'notice_can_install_required'     			=> _n_noop( 'PMPro bbPress requires the following plugin: %1$s.', 'PMPro bbPress requires the following plugins: %1$s.' ), // %1$s = plugin name(s)
+			'notice_can_install_recommended'			=> _n_noop( 'PMPro bbPress recommends the following plugin: %1$s.', 'PMPro bbPress recommends the following plugins: %1$s.' ), // %1$s = plugin name(s)
+			'notice_cannot_install'  					=> _n_noop( 'PMPro bbPress: Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'PMPro bbPress: Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.' ), // %1$s = plugin name(s)
+			'notice_can_activate_required'    			=> _n_noop( 'PMPro bbPress: The following required plugin is currently inactive: %1$s.', 'PMPro bbPress: The following required plugins are currently inactive: %1$s.' ), // %1$s = plugin name(s)
+			'notice_can_activate_recommended'			=> _n_noop( 'PMPro bbPress: The following recommended plugin is currently inactive: %1$s.', 'PMPro bbPress: The following recommended plugins are currently inactive: %1$s.' ), // %1$s = plugin name(s)
+			'notice_cannot_activate' 					=> _n_noop( 'PMPro bbPress: Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'PMPro bbPress: Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.' ), // %1$s = plugin name(s)
+			'notice_ask_to_update' 						=> _n_noop( 'PMPro bbPress: The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'PMPro bbPress: The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.' ), // %1$s = plugin name(s)
+			'notice_cannot_update' 						=> _n_noop( 'PMPro bbPress: Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'PMPro bbPress: Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.' ), // %1$s = plugin name(s)
 			'install_link' 					  			=> _n_noop( 'Begin installing plugin', 'Begin installing plugins' ),
 			'activate_link' 				  			=> _n_noop( 'Activate installed plugin', 'Activate installed plugins' ),
 			'return'                           			=> __( 'Return to Required Plugins Installer', $theme_text_domain ),
@@ -107,7 +107,7 @@ function pmprobbp_check_forum() {
 	if( ! bbp_is_forum_archive() && ! empty( $forum_id ) && pmpro_bbp_is_forum() ) {			
 		// The current user does not have access to this forum, re-direct them away
 		if( ! pmpro_has_membership_access( $forum_id ) ) {
-			wp_redirect( get_post_type_archive_link( 'forum' ) );
+			wp_redirect( get_post_type_archive_link( 'forum' ) . '&noaccess=1' );
 			exit;
 		}
 	}
@@ -152,3 +152,15 @@ function pmpro_bbp_is_forum( $forum_id = NULL ) {
 	else
 		return false;
 }
+
+/* Add membership level required message if user does not have access */
+function pmpro_bbp_membership_msg() 
+{
+    $pmpro_bbp_error_msg = 'You do not have the required membership level to access that forum.'; // error message to display
+
+    if (bbp_is_forum_archive() && !empty($_REQUEST['noaccess'])) 
+	{
+        echo '<p class="pmpro_bbp_membership_msg">' . $pmpro_bbp_error_msg . '</p>';
+    }
+}
+add_action('bbp_template_before_forums_index','pmpro_bbp_membership_msg');
---
+++ readme.txt
@@ -0,0 +1,33 @@
+=== Paid Memberships Pro BBPress Addon ===
+Contributors: strangerstudios, slocumstudio
+Tags: paid memberships pro, bbpress, forums, membership forum, restrict forum
+Requires at least: 3.5
+Tested up to: 3.8
+Stable tag: 1.0
+
+Paid Memberships Pro BBPress Addon
+
+== Description ==
+
+Integrates Paid Memberships Pro with BBPress to restrict forum content based on PMPro membership level.
+
+Requires BBPress and Paid Memberships Pro installed and activated.
+
+== Installation ==
+
+= Prerequisites =
+1. You must have Paid Memberships Pro and BBPress installed and activated on your site.
+
+= Download, Install and Activate! =
+1. Download the latest version of the plugin.
+2. Unzip the downloaded file to your computer.
+3. Upload the /pmpro-bbpress/ directory to the /wp-content/plugins/ directory of your site.
+4. Activate the plugin through the 'Plugins' menu in WordPress.
+
+== Usage ==
+After activation, navigate to the "Edit Forum" page of any forum you have already created. In the right sidebar with the Publishing options, you will notice a new panel titled "Require Membership". Simply check the boxes for the required membership levels to access this forum, then click "Update" (or "Publish" if you are creating a new forum).
+
+== Changelog == 
+
+= 1.0 =
+* Initial WP.org release.
